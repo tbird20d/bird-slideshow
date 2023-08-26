56a04eda5568a29f407e9d0f2aca18addce29a0c
Nathan Bird <nathanbirdka@gmail.com>
2023-08-26 16:59:19 -0600

    Add utility batch files for easier windows testing

---
+++ tagger/populate-sample-db.bat
@@ -0,0 +1,21 @@
+@ECHO OFF
+SET DEBUG=%1
+CHDIR C:\Users\natha\Programming\Personal\GitHubProjects\nbird11_bird-slideshow\tagger
+IF NOT EXIST %LOCALAPPDATA%\tagger\dist\tagger\tagger.exe (
+    ECHO Did not find `tagger.exe`.
+    ECHO Building executable...
+    CALL .\build-executable.bat
+) ELSE (
+    ECHO Found `tagger.exe`.
+)
+@ECHO ON
+tagger remove-database %DEBUG%
+tagger init %DEBUG%
+@CHDIR .\sample-pics
+tagger tag tree -- ropes-zipline-course.JPG Tori-gate-japan.jpg %DEBUG%
+tagger tag outside -- ropes-zipline-course.JPG Tori-gate-japan.jpg skyscraper.jpg %DEBUG%
+tagger tag building -- skyscraper.jpg %DEBUG%
+tagger tag japan -- Tori-gate-japan.jpg %DEBUG%
+tagger tag food -- dessert-swans.JPG %DEBUG%
+
+sqlite3 %LOCALAPPDATA%\tagger\tagger.db .dump
